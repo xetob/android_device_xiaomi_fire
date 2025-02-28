ad594d6605f7e90857343af2399ec3899309b9b8
xetob <xetob1122@gmail.com>
2025-02-28 23:37:03 +0300

    fire: fix typo

--- lineage_fire.mk
+++ lineage_fire.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from alioth device
+# Inherit from fire device
 $(call inherit-product, device/xiaomi/fire/device.mk)
 
 PRODUCT_NAME := lineage_fire
