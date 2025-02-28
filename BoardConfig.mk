cf43ad49b361ff87ef9fc0548f7d5606bc4ce62f
xetob <xetob1122@gmail.com>
2025-02-28 18:26:19 +0300

    fire: Initial skeleton tree

---
+++ BoardConfig.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/fire
+
+
+# Inherit the proprietary files
+include vendor/xiaomi/fire/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/fire/fire-vendor.mk)
