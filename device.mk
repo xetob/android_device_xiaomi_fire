303ee0a7259b91020b9641c1d2d9ca0b6616723d
xetob <xetob1122@gmail.com>
2025-05-12 16:28:02 +0300

    switch to mtk plpath utils

--- device.mk
+++ device.mk
@@ -14,22 +14,25 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Update Engine & Update Verifier 
+# A/B
 PRODUCT_PACKAGES += \
     update_engine \
+    update_engine_sideload \
     update_verifier \
-    update_engine_sideload
+    otapreopt_script \
+    checkpoint_gc
 
-# A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE) \
     POSTINSTALL_OPTIONAL_system=true
 
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=$(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE) \
+    POSTINSTALL_OPTIONAL_vendor=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -44,18 +47,15 @@ PRODUCT_PACKAGES += \
     bootctrl.mt6768 \
     bootctrl.mt6768.recovery
 
+# MTK plpath utils
 PRODUCT_PACKAGES += \
-    create_pl_dev \
-    create_pl_dev.recovery
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery
 
 # Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
-
-# Soong
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
 
 # Fastbootd
 PRODUCT_PACKAGES += \
@@ -68,7 +68,7 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 33
 
-# Additional Libraries
+# Keymaster
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
     libkeymaster41 \
